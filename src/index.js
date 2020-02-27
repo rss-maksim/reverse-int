01cbc55eea5d98ae662c960c09f5658b3bcf0d70
maksim.kirilko <maksimkirilko@gmail.com>
2020-02-27 16:48:46 +0300

    feat: implement reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+/** Solution #1 */
+
 module.exports = function reverse (n) {
-  
-}
+    return Number(String(Math.abs(n)).split('').reverse().join(''));
+};
+
+/** Solution #2 */
+
+module.exports = function reverse2 (n) {
+    const numberString = String(Math.abs(n));
+    const tepmArray = [];
+    for (let i = numberString.length - 1; i >= 0; i--) {
+        tepmArray.push(numberString[i])
+    }
+    return Number(tepmArray.join(''));
+};
